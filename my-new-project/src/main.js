3a735a353bb457f17cdc9d4b6545a2b728dc2e9f
Sandeep <off33wor33@gmail.com>
2026-08-14 00:08:03 +0530

    Frontend created

--- my-new-project/src/main.js
+++ my-new-project/src/main.js
@@ -1,60 +1,158 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// 1. Create a scene
+const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const camera = new THREE.PerspectiveCamera(
+  95,
+  window.innerWidth / window.innerHeight,
+  1,
+  1200
+);
 
-<div class="ticks"></div>
+// 3. Create a renderer
+const renderer = new THREE.WebGLRenderer();
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+document.body.appendChild(renderer.domElement);
 
-setupCounter(document.querySelector('#counter'))
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+const blackMaterial = new THREE.MeshStandardMaterial({
+    color: 0x2e2e2d
+});
+
+const redMaterial = new THREE.MeshStandardMaterial({
+    color: "red"
+
+});
+
+const orangeMaterial = new THREE.MeshStandardMaterial({
+    color: "magenta"
+});
+
+const whiteMaterial = new THREE.MeshStandardMaterial({
+    color: "white"
+});
+
+const yellowMaterial = new THREE.MeshStandardMaterial({
+    color: "yellow"
+});
+
+const greenMaterial = new THREE.MeshStandardMaterial({
+    color: "green"
+});
+
+const blueMaterial = new THREE.MeshStandardMaterial({
+    color:"cyan"
+});
+
+const spacing =1.035;
+
+const edges = new THREE.EdgesGeometry(geometry);
+
+
+for (let x = -1; x <= 1; x++) {
+
+    for (let y = -1; y <= 1; y++) {
+
+        for (let z = -1; z <= 1; z++) {
+
+            if (x === 0 && y === 0 && z === 0) {
+                continue;
+            }
+
+            const cubieMaterials = [
+                blackMaterial,
+                blackMaterial,
+                blackMaterial,
+                blackMaterial,
+                blackMaterial,
+                blackMaterial
+            ];
+
+            if (x === 1) {
+                cubieMaterials[0] = redMaterial;
+            }
+            if (x === -1) {
+                cubieMaterials[1] = orangeMaterial;
+            }
+            if (y === 1) {
+                cubieMaterials[2] = yellowMaterial;
+            }
+            if (y === -1) {
+                cubieMaterials[3] = whiteMaterial;
+            }
+            if (z === 1) {
+                cubieMaterials[4] = greenMaterial;
+            }
+            if (z === -1) {
+                cubieMaterials[5] = blueMaterial;
+            }
+            const cube = new THREE.Mesh(
+                geometry,
+                cubieMaterials
+            );
+            cube.position.set(
+                x * spacing,
+                y * spacing,
+                z * spacing
+            );
+            const border = new THREE.LineSegments(
+                edges,
+                new THREE.LineBasicMaterial({
+                    color: 0x000000
+                })
+            );
+            cube.add(border);
+            scene.add(cube);
+        }
+    }
+}
+scene.background = new THREE.Color(0x1c1c1b);
+const floorGrid = new THREE.GridHelper(
+    1050,
+    350,
+    0x444444,
+    0x222222
+);
+
+scene.add(floorGrid);
+floorGrid.position.y = -15;
+
+const light1 = new THREE.DirectionalLight(
+    0xffffff,
+    3
+);
+const light2 = new THREE.DirectionalLight(
+    0xffffff,
+    3
+);
+
+light1.position.set(-5, 6, 2);
+light2.position.set(5,-6,2);
+scene.add(light1);
+scene.add(light2);
+
+camera.position.set(6, 6, 6);
+camera.lookAt(0, 0, 0);
+
+const ambientLight = new THREE.AmbientLight("white", 10);
+scene.add(ambientLight);
+
+const controls = new OrbitControls(
+    camera,
+    renderer.domElement
+);
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+function animate() {
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+animate();
